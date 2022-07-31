b45f241920e48f4303117db8bd43181e0d010d08
Jesper Petersen <info@ordklar.dk>
2022-07-31 21:13:38 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿var users = new List<string>
+{
+    "Jesper Petersen",
+    "Kasper Olsen",
+    "Michael Willumsen"
+};
+
+var firstOne = users.First(); //Jesper Petersen
+Console.WriteLine(firstOne);
+
+var firstTwo = users.First(r => r.StartsWith("Michael")); // Michael Willumsen
+Console.WriteLine(firstTwo);
