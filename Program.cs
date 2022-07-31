d35f139d73dbfcc33e3496fd96c0cb3e683f09e8
Jesper Petersen <info@ordklar.dk>
2022-07-31 21:18:32 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,13 @@
     "Kasper Olsen",
     "Michael Willumsen"
 };
+var number = new List<int>
+{
+    51
+};
 
+
+//First
 var firstOne = users.First(); //Jesper Petersen
 Console.WriteLine(firstOne);
 
@@ -13,3 +19,14 @@ Console.WriteLine(firstTwo);
 
 var firstOrDefaultOne = users.FirstOrDefault(r => r.StartsWith("Kasm"), "Intet!");
 Console.WriteLine(firstOrDefaultOne);
+
+
+//Single
+var singleOne = number.Single();
+Console.WriteLine(singleOne);
+
+var singleTwo = users.Single(r => r.StartsWith("Kasper"));
+Console.WriteLine(singleTwo);
+
+var singleOrDefaultOne = users.SingleOrDefault(r => r.StartsWith("asf"), "Intet");
+Console.WriteLine(singleOrDefaultOne);
